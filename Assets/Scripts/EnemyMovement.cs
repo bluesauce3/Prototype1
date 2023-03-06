dc291c6a1bf597886b517f402351339c71b2c39a
Theo <106856604+bluesauce3@users.noreply.github.com>
2023-03-07 10:13:51 +1300

    Initial commit

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+    public System.Random rnd;
+    public int pos;
+    public float resetEnemyTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resetEnemyTimer = 0;
+        rnd = new System.Random();
+        pos = rnd.Next(-1, 2) * 5;
+        transform.position = new Vector3(pos, 0, 150);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos = rnd.Next(-1, 2) * 5;
+        resetEnemyTimer += Time.deltaTime;
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        if (resetEnemyTimer > 5)
+        {
+            transform.position = new Vector3(pos, 0, 150);
+            resetEnemyTimer = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 50.0f;
+    private float turnSpeed = 50.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    public bool restart;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        restart = Input.GetButtonDown("Fire1");
+        //Move the vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //Rotates the vehicle based on horizontal input
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+        if (restart)
+        {
+            transform.positiion = new Vector3(0, 0, 0); //???
+        }
+    }
+}
